fe31a3d81ef162f743fecc48176aba7d747f7ebc
almayce <kolchsta@gmail.com>
2019-01-07 21:41:22 +0200

    updated open vpn script

--- bin/main.py
+++ bin/main.py
@@ -5,39 +5,40 @@ import time
 from pathlib import Path
 
 home = str(Path.home())
-separator = os.sep
 
 
 def find_directory_by_end_of_file_path(end_of_path):
     separated_end_of_path = os.path.join(end_of_path)
 
     for root, dirs, files in os.walk(home):
+        if "/." in root:
+            continue
         for f in files:
             file_path = root + "/" + f
             try:
                 if file_path.endswith(separated_end_of_path):
-                    return os.path.join(root)
+                    return replace_separator(os.path.join(root))
             except UnicodeEncodeError:
                 encoded_file_path = file_path.encode('utf-8', 'surrogateescape').decode('ISO-8859-1')
                 if encoded_file_path.endswith(separated_end_of_path):
-                    return os.path.join(root)
+                    return replace_separator(os.path.join(root))
 
     raise FileNotFoundError
 
 
 def replace_separator(path):
-    path.replace("/", separator)
+    return path.replace("/", os.sep)
 
 
 def connect_to_vpn():
-    print("Helper: Connecting to VPN...")
     subprocess.Popen(['sudo', 'killall', 'openvpn'],
                      cwd=home,
                      stdin=None,
                      stdout=open(os.devnull, 'wb'),
                      stderr=open(os.devnull, 'wb'),
-                     close_fds=True)
-    time.sleep(4)
+                     close_fds=True).wait(3.0)
+
+    print("Helper: Connecting to VPN...")
     subprocess.Popen(['sudo', 'openvpn', '--config', 'client.ovpn'],
                      cwd=home,
                      stdin=None,
@@ -46,7 +47,7 @@ def connect_to_vpn():
                      close_fds=True)
 
 
-def run_selenoid(project):
+def run_selenoid():
     print("Helper: Running Selenoid...")
     subprocess.Popen(['./selenoid -container-network bridge1'],
                      shell=True,
@@ -88,7 +89,7 @@ def open_hubstaff():
     print("Helper: Opening Hubstaff...")
     subprocess.Popen(["./HubstaffClient.bin.x86_64"],
                      shell=True,
-                     cwd=find_directory_by_end_of_file_path("HubstaffClient.bin"),
+                     cwd=find_directory_by_end_of_file_path("HubstaffClient.bin.x86_64"),
                      stdin=None,
                      stdout=open(os.devnull, 'wb'),
                      stderr=open(os.devnull, 'wb'),
@@ -118,7 +119,7 @@ def open_file_manager():
 
 
 def welcome():
-    print("Welcome to helper\n")
+    print("\nWelcome to helper!\n")
 
 
 welcome()
@@ -136,7 +137,7 @@ if '1' in input_string:
     connect_to_vpn()
 
 if '2' in input_string:
-    run_selenoid("bpm-automation-testing-master")
+    run_selenoid()
 
 if '3' in input_string:
     open_browser()
